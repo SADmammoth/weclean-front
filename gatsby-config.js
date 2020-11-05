1f196dfc24ba13a68c7f8b3cb5c3e53771e0b079
SADmammoth <lo.ma200018@gmail.com>
2020-11-05 20:44:56 +0300

    feat: fetch data from graphql server

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        fieldName: `graphqlData`,
+        url: process.env.GATSBY_API_URL,
+        typeName: `VacuumCleaner`,
+        refetchInterval: 60,
+        batch: true,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,34 @@
+import { graphql } from "gatsby"
 import React from "react"
 
-export default function Home() {
+export default function Home({
+  data: {
+    graphqlData: { allVacuumCleaners },
+  },
+}) {
+  console.log(allVacuumCleaners)
   return <div>Hello world!</div>
 }
+
+export const query = graphql`
+  query {
+    graphqlData {
+      allVacuumCleaners {
+        id
+        cleaningFeatures
+        color
+        construction
+        dustCollectorType
+        manufacturer
+        model
+        noiseLevel
+        powerConsumption
+        powerCordLength
+        powerSource
+        price
+        volumeOfDustCollector
+        weight
+      }
+    }
+  }
+`
